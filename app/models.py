a5365134d79e5df6752d16e6fdb1c4346ba9934d
al <alenalexkuz@mail.com>
2020-06-16 18:46:28 +0300

    before The Flask Mega-Tutorial Part XI: Facelift

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from time import time
+import jwt
+from app import app
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -29,6 +32,20 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def followed_posts(self):
         followed = Post.query.join(
